c1dd506dfc49d23b6e37aa80f20dd45d67b019e0
Duvan Cruz <40447935+dvncrz@users.noreply.github.com>
2021-03-11 14:16:59 -0500

    Update audioplayer.js

--- audioplayer.js
+++ audioplayer.js
@@ -1,3 +1,4 @@
+/*SCRIPT WRITTEN BY DUVAN CRUZ FOR 6CONNEX*/
 let audio = document.getElementById('audio');
 let isPaused = audio.paused;
 let isPlaying = !isPaused;
@@ -6,24 +7,20 @@ var songId = 0;
 
 var playlist = [];
 
-/*playlist.push(["https://cdn-akamai.6connex.com//465/2156//Lobby_Playlist_BMS_Prospanica_16141150836837815.mp3","Simples corazones","Fonseca"]);
-
-playlist.push(["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3","Helix Sound","Unknown Artist"]);*/
-
 playlist.push(["https://www.mboxdrive.com/Rising%20World%20by%20Chill%20Study.mp3","Rising World","Chill Study"]);
 playlist.push(["https://www.mboxdrive.com/Beach%20by%20JAM%20Studio.mp3","Beach","JAM Studio"]);
 
 function playpause() {       
     var element = document.getElementById("playpause_btn")
 
-    if (element.matches(".fa-play")) {
-      element.classList.remove("fa-play");
-      element.classList.add("fa-pause");
+    if (element.matches(".fa-play-circle")) {
+      element.classList.remove("fa-play-circle");
+      element.classList.add("fa-pause-circle");
       audio.play();
       document.getElementById("equalizer").style.visibility = "visible";
-    } else if (element.matches(".fa-pause")) {
-      element.classList.remove("fa-pause");
-      element.classList.add("fa-play");
+    } else if (element.matches(".fa-pause-circle")) {
+      element.classList.remove("fa-pause-circle");
+      element.classList.add("fa-play-circle");
       audio.pause();
       document.getElementById("equalizer").style.visibility = "hidden";
     } 
@@ -47,16 +44,74 @@ function backward() {
     autoplay(songId);
 }
 
+audio.addEventListener('ended', (event) => {
+  forward();
+});
+
+var previousvol = 1
+
+function mute() {
+	var elementvol = document.getElementById("mute_btn")
+    
+	if (audio.volume > 0) {
+    	previousvol = audio.volume;    	
+        audio.volume = 0;
+        elementvol.classList.remove("fa-volume-up");
+    	elementvol.classList.add("fa-volume-off");
+    } else if (audio.volume == 0) {
+    	audio.volume = previousvol;
+        elementvol.classList.remove("fa-volume-off");
+    	elementvol.classList.add("fa-volume-up");
+    }
+    document.getElementById("volume").innerText = Math.floor(audio.volume * 100);
+}
+
+function volumeup() {
+	var elementvol = document.getElementById("mute_btn");
+    
+    previousvol += 0.05;
+    
+    if (previousvol > 1) {
+    	previousvol = 1;
+    }
+    
+	audio.volume = previousvol;
+    document.getElementById("volume").innerText = Math.round(previousvol * 100);
+    if ((Math.floor(previousvol * 100)) > 0) {
+    	elementvol.classList.remove("fa-volume-off");
+    	elementvol.classList.add("fa-volume-up");
+    } 
+}
+
+function volumedown() {
+	var elementvol = document.getElementById("mute_btn");
+    
+    previousvol -= 0.05;
+    
+    if (previousvol < 0) {
+    	previousvol = 0;
+    }
+    
+	audio.volume = previousvol;
+    document.getElementById("volume").innerText = Math.round(previousvol * 100);
+    if ((Math.floor(audio.volume * 100)) == 0) {
+    	elementvol.classList.remove("fa-volume-up");
+    	elementvol.classList.add("fa-volume-off");
+    }    	
+}
+
+
 window.onload = autoplay(songId);
 
 function autoplay(song) {
     loadsong(song);
+    document.getElementById("volume").innerText = Math.round(audio.volume * 100);
 
     var element = document.getElementById("playpause_btn")
 
     audio.play();
-    element.classList.remove("fa-play");
-    element.classList.add("fa-pause");
+    element.classList.remove("fa-play-circle");
+    element.classList.add("fa-pause-circle");
     document.getElementById("equalizer").style.visibility = "visible";
 }
 
